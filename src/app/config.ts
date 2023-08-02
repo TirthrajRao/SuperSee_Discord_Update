aca7e81845da48a4c20243e79dd2efc6deae1f96
TirthrajRao <tirthrajrao2394@gmail.com>
2023-08-02 11:03:27 +0530

    baseurl

--- src/app/config.ts
+++ src/app/config.ts
@@ -4,8 +4,8 @@
 // const baseUrl = "http://192.168.0.166:3000/"; //Shukla's Home
 // const socketUrl = "http://192.168.0.166:3000"; //Shukla's Home
 
-const baseUrl = "https://raodoctor.raoinfo.tech:4444/"; // Rajkot Office
-const socketUrl = "https://raodoctor.raoinfo.tech:4444"; // Rajkot Office
+const baseUrl = "https://vk.supersee.io:5555/"; // Rajkot Office
+const socketUrl = "https://vk.supersee.io:5555"; // Rajkot Office
 
 export const config = {
   baseApiUrl: baseUrl,
